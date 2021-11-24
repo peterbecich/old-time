6d1b00c6353c574af8d4f517d54ea70668ff4cf2
Peter Becich <peterbecich@gmail.com>
2021-11-24 14:04:24 -0800

    autoreconf override

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 # https://input-output-hk.github.io/haskell.nix/tutorials/getting-started/
 { compiler ? "ghc8107",
   ghcVersion ? "8.10.7",
-  withCoverage ? false
+  withCoverage ? false,
+
 }:
   let
     sources = import ./nix/sources.nix {};
@@ -12,8 +13,16 @@
 
   in
     pkgs.haskell-nix.project {
+      modules = [
+        {
+          packages.old-time.components.library.preConfigure = ''
+            ${pkgs.autoconf}/bin/autoreconf -i
+          '';
+        }
+      ];
 
       src = pkgs.haskell-nix.haskellLib.cleanGit {
+
         name = "old-time";
         src = ./.;
       };
